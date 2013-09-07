a8e65ea8960addffcdc77bebd77cc94233913447
Natasha Murashev <nmurashev@gmail.com>
2013-09-07 09:49:16 -0700

    added test coverage for omniauth creating a user with github

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe ".create_with_omniauth" do
+    it "successfully creates a user from the omniauth response" do
+      expect {
+        User.create_with_omniauth(OmniAuth.config.mock_auth[:github])
+      }.to change{ User.count }.by(1)
+    end
+    it "correctly assigns all the attributes" do
+      user = User.create_with_omniauth(OmniAuth.config.mock_auth[:github])
+      user.provider.should eq 'github'
+      user.uid.should eq '12345'
+      user.name.should eq 'natasha'
+      user.gravatar_id.should eq '123456789'
+      user.location.should eq 'San Francisco'
+      user.nickname.should eq 'NatashaTheRobot'
+      user.email.should eq 'hi@natashatherobot.com'
+    end
+  end
+
 end
